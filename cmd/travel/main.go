e06b54d8996e3c5994650163a59f0208115943e6
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2021-12-27 11:27:57 +0100

    recommendations endpoint and cors function

--- cmd/travel/main.go
+++ cmd/travel/main.go
@@ -4,13 +4,44 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"travel-buddy/travel"
+	"strconv"
+	"strings"
+	"travel-buddy/travel"	
 )
 
 func main() {
-	http.HandleFunc("/journeys", func(responseWriter http.ResponseWriter, req *http.Request) {
+	
+	http.HandleFunc("/journeys", cors(func(responseWriter http.ResponseWriter, req *http.Request) {
 		respond(responseWriter, req, travel.Journeys)
-	})
+	}))
+
+	http.HandleFunc("/recommendations", cors(func(responseWriter http.ResponseWriter, req *http.Request) {
+		query := &travel.Query{
+			Journey: strings.Split(req.URL.Query().Get("journey"), "|"),
+		}
+		var err error
+
+		query.Lat, err = strconv.ParseFloat(req.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(responseWriter, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		query.Lng, err = strconv.ParseFloat(req.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(responseWriter, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		query.Radius, err = strconv.Atoi(req.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(responseWriter, err.Error(), http.StatusBadRequest)
+			return
+		}
+		query.CostRangeStr = req.URL.Query().Get("cost")
+		places := query.Run()
+		respond(responseWriter, req, places)
+	}))
 
 	fmt.Println("Listening and Serving at :8080")
 	http.ListenAndServe(":8080", http.DefaultServeMux)
@@ -22,4 +53,11 @@ func respond(writer http.ResponseWriter, req *http.Request, data []interface{}) 
 		publicData[index] = travel.Public(value)
 	}
 	return json.NewEncoder(writer).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, req *http.Request) {
+		writer.Header().Set("Access-Control-Allow-Origin", "*")
+		f(writer, req)
+	}
 }
